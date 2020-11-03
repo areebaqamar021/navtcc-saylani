7954077c8a7903037d0615a34ca90d96ca9979a6
areebaqamar021 <areebaqamar021@gmail.com>
2020-11-03 13:00:31 +0500

    new work

---
+++ Class#8/index.js
@@ -0,0 +1,56 @@
+// var str = "the quick brown fox jumps over the lazy dog";
+// console.log(str.indexOf('quick'));
+// console.log(str.lastIndexOf('quick'));
+// console.log(str.charAt(2));
+
+// var sliceToReplace = str.slice(0,19);
+// var replaceStr = "the white cat";
+// var newStr = replaceStr + str.slice(19);
+// console.log(newStr);
+
+// var start = str.indexOf('the quick brown fox');
+// var replace = 'the quick brown fox';
+// var end = start + replace.length;
+// var newText = str.slice(0, start) + replaceStr + str.slice(end);
+// console.log(newText);
+
+// str = str.replace(/the quick brown fox/g, 'the white cat');
+// console.log(str);
+
+// var find = "!";
+// var str = "Hello World!";
+// var res = str.indexOf('!');
+// if(res !== -1) {
+//     console.log(`! found at address ${res}`);
+// }
+
+// var str = "Karac  hi";
+// var numChars = str.length;
+// for (var i = 0; i < numChars; i++) {
+//     if (str.slice(i, i + 2) === "  ") {
+//        alert("Double spaces are found!");
+//         break;
+//     }
+// } 
+
+// var str = prompt("Enter some text");
+// var numChars = str.length;
+// for (var i = 0; i < numChars; i++) {
+//     if (str.slice(i, i + 2) === "  ") {
+//        alert("Double spaces are found!");
+//         break;
+//     }
+// }
+
+// var input = prompt('enter your num');
+// console.log(typeof input);
+//  var result = parseInt(input) + 22;
+//  console.log("result", result);
+
+//  function roundOf(num, num1){
+//      var num = num.toFixed(num1);
+//      console.log(num);
+//  }
+// roundOf(4.80954758769, 4);
+// roundOf(4.80954758769, 3);
+// roundOf(4.80954758769, 2);
