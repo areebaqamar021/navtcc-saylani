fbf8fa1c90fe209721cd7f493aa25ef02ce7721c
hr1143177 <he1143177@gmail.com>
2020-11-16 12:54:54 +0500

    todays class

---
+++ 16-Nov-2020/index.js
@@ -0,0 +1,25 @@
+// var num3;
+// let square = (num) => {
+//     num3= num * num;
+// }
+// square(6);
+// console.log(num3);
+
+var arr = [2,6,8,9]
+
+const newArr = arr.map(
+    (item) => {
+        return item *5;
+    }
+)
+console.log(newArr);
+
+var arr1 = [1, 4, 6, 7, 8]
+const newArr1 = arr1.map(
+    (val1) => {
+        return Math.sqrt(val1)
+    }
+)
+console.log(newArr1);
+
+
