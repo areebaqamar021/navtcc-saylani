64a25882f6a3a284cb6f9f387707a46f9074e44a
hr1143177 <he1143177@gmail.com>
2020-11-17 13:06:23 +0500

    updated work

---
+++ 17-Nov-2020/index.js
@@ -0,0 +1,30 @@
+// var teachers = ['faiza','inzimam','Ali Mughal'];
+// var students = ['areeba','hifsa','komal','tooba'];
+// var age = [16, 18, 20, 26];
+
+// // var a = teachers.includes('faiza');
+// // console.log(a);
+
+// let find = teachers.find((item)=> {
+//     return item == 'faiza';
+// })
+// if(find) {
+//     let res = students.map((item,index)=>{
+//         return {name:item, age:age[index]}
+//     })
+//     console.log(res)
+// }
+
+// var Iam = {
+//     name: 'areeba',
+//     joinDate: '17=feb-2020',
+//     age: 18,
+//     course: 'web saylani'
+// }
+// console.log(Iam)
+
+// var total= prompt('Enter salary');
+// var salary = (total >= 5000) ? '1000'
+//            :(total < 5000) ? '500'
+//            :invalid;
+// console.log(salary);
